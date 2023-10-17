0fe36c311a29adc004aece9187d4e931e75bbaf8
Andrew Yaakoub <39156362+Andrew2120@users.noreply.github.com>
2023-10-17 20:35:01 +0300

    change

--- fiber/src/main/java/com/b_labs/fiber/Design.kt
+++ fiber/src/main/java/com/b_labs/fiber/Design.kt
@@ -1,18 +1,27 @@
 package com.b_labs.fiber
 
-import DesignSystem
-import androidx.compose.foundation.background
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
-class Design(private val design: DesignSystem? = null) {
+class Design(private val design: DesignTheme) {
 
     @Composable
     fun PrimaryButton() {
-        Button(onClick = { /*TODO*/ }, modifier = Modifier.background(design?.color?.green?.green0!!)) {
+        Button(
+            onClick = { /*TODO*/ },
+
+            colors = ButtonDefaults.buttonColors(containerColor = design.primaryColor),
+
+        ) {
             Text(text = "Click")
         }
     }
 }
+
+data class DesignTheme(
+    val primaryColor: Color,
+    val secondaryColor: Color,
+)
--- fiber/src/main/java/com/b_labs/fiber/DesignIn.kt
+++ fiber/src/main/java/com/b_labs/fiber/DesignIn.kt
@@ -1,11 +1,3 @@
 package com.b_labs.fiber
 
-import androidx.compose.foundation.background
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-interface DesignIn {
-
-}
+interface DesignIn
